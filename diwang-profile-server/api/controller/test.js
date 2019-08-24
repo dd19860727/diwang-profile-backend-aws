11fc5c201d9f45fad33f2a97767d8296b23a6f54
Di Wang <dd19860727@gmail.com>
2019-08-24 17:13:54 -0400

    create a reusable create token function add create token api for user login optimize api global variable change testpost api that add username and password add interface get userById finished the api: get test data by id add curd api create public db response and erro function refactor the code

--- diwang-profile-server/api/controller/test.js
+++ diwang-profile-server/api/controller/test.js
@@ -1,20 +1,71 @@
-const test = require("../models/test");
-const jwt = require("jsonwebtoken");
+const Test = require("../models/test");
+const mongoose = require('mongoose');
 const authController = require('./auth/authController');
+const errorController = require('./errors/errorController');
+const dbController = require('./db/dbController');
 
 exports.test_get_all = (req, res) => {
-     const resObj = {message: 'Test'};
-     authController.jwt_verify(req, res, resObj);
+    Test.find().exec()
+        .then(docs => {
+            if (docs.length) {
+                authController.jwt_verify(req, res, docs);
+            } else {
+                res.status(404).json({
+                    message: 'No entries found'
+                })
+            }
+        }).catch(err => {
+            errorController.db_error_handler(res, err);
+        });
 };
 
 exports.test_post = (req, res) => {
-    const test = {
+    const test = new Test({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        id: req.body.id
-    };
+        password: req.body.password
+    });
+
+    test.save().catch(err => {
+        errorController.db_error_handler(res, err);
+    });
+
     res.status(200).json({
         message: "Post Successfully",
         createPost: test
     });
 };
 
+
+exports.test_get_by_id = (req, res) => {
+    const id = req.params.testId;
+    Test.findById(id).exec().then(doc => {
+        if (doc) {
+            dbController.success_retrun(res, doc);
+        } else {
+            res.status(404).json({ message: "No valid entry found" });
+        }
+
+    }).catch(err => {
+        errorController.db_error_handler(res, err);
+    });
+};
+
+exports.test_update = (req, res) => {
+    const id = req.params.testId;
+    const updateParams = req.body;
+    Test.update({ _id: id }, { $set: updateParams }).exec().then(result => {
+        dbController.success_retrun(res, result);
+    }).catch(err => {
+        errorController.db_error_handler(res, err);
+    });
+};
+
+exports.test_delete = (req, res) => {
+    const id = req.params.testId;
+    Test.remove({ _id: id }).exec().then(result => {
+        dbController.success_retrun(res, result);
+    }).catch(err => {
+        errorController.db_error_handler(res, err);
+    });
+};
--- diwang-profile-server/api/routers/api/apis.js
+++ diwang-profile-server/api/routers/api/apis.js
@@ -1,11 +1,15 @@
-const express = require("../../../node_modules/express");
+const express = require("express");
 const apiRouter = express.Router();
 const testController = require('../../controller/test');
 const authController = require('../../controller/auth/authController');
 
 apiRouter.get("/test", authController.verify_token, testController.test_get_all);
 apiRouter.post("/testpost", testController.test_post);
+apiRouter.get("/getId/:testId", testController.test_get_by_id);
+apiRouter.patch("/updateId/:testId", testController.test_update);
+apiRouter.delete("/deleteId/:testId", testController.test_delete);
 
 apiRouter.get("/token", authController.req_token);
+apiRouter.post("/create-token", authController.create_token_api);
 
 module.exports = apiRouter;
